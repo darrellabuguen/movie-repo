cfde0f812d4d16aec15e09409f1c49cde1cf69ad
darrellabuguen <darrellabuguen2@gmail.com>
2024-05-21 23:11:03 +0800

    added height limit to celeb biography in celebinfo jsx

--- src/pages/CelebrityInfo.jsx
+++ src/pages/CelebrityInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../components/useFetch';
 import MovieCredits from '../components/MovieCredits';
@@ -8,6 +8,24 @@ const CelebrityInfo = () => {
     const { data, loading, error } = useFetch(`https://api.themoviedb.org/3/person/${celebid}?language=en-US`, "GET");
     var title = document.querySelector("title");
     title.innerText = `${celebname} | Celebrity Info`;
+    const [con_height, setHeight] = useState("h-16");
+    const [visible, setVisibility] = useState("block");
+    const [more, setMore] = useState("See More");
+    const checkDescriptionHeight = () => {
+        const description_con = document.querySelector(".desc_con");
+        const description = document.querySelector(".desc");
+        if (description) {
+            if (description.scrollHeight == description_con.scrollHeight) {
+                setVisibility("");
+            } else {
+                setVisibility("hidden")
+            };
+        }
+    }
+    useEffect(() => {
+        checkDescriptionHeight();
+        window.addEventListener("resize", checkDescriptionHeight);
+    });
 
     return (
         <div className='mx-auto max-w-7xl  p-6 lg:px-8'>
@@ -20,7 +38,23 @@ const CelebrityInfo = () => {
                         <img src={"https://image.tmdb.org/t/p/w300/" + data.profile_path} alt='img' className='h-full' />
                         <div>
                             <h1>Biography:</h1>
-                            <p className=' text-gray-300'>{data.biography}</p>
+                            <div className={`desc_con overflow-hidden ${con_height}`}>
+                                <p className=' text-gray-300 desc'>{data.biography}</p>
+                            </div>
+                            <div
+                                className={`text-blue-500 cursor-pointer ${visible}`}
+                                onClick={() => {
+                                    if (con_height === "h-16") {
+                                        setHeight("max-h-full");
+                                        setMore("See Less");
+                                    } else {
+                                        setHeight("h-16");
+                                        setMore("See More");
+                                    }
+                                }}
+                            >
+                                {more}
+                            </div>
                         </div>
                     </div>
                     <MovieCredits id={celebid} name={celebname} />
