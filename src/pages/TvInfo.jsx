30959c4e42e2b185d69a79a35b486a5a3715918b
darrellabuguen <darrellabuguen2@gmail.com>
2024-05-22 03:38:32 +0800

    added placeholder image for tvinfo jsx

--- src/pages/TvInfo.jsx
+++ src/pages/TvInfo.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../components/useFetch';
 import Cast from '../components/Cast';
+import mvimage from "../assets/nopic-movie-banner.jpg";
 
 const TvInfo = () => {
     const { tvname, tvid } = useParams();
     const { data, loading, error } = useFetch(`https://api.themoviedb.org/3/tv/${tvid}?language=en-US`, "GET");
     var title = document.querySelector("title");
     title.innerText = `${tvname} | TV Info`; //change the title
+    const img_condition = "https://image.tmdb.org/t/p/original/null";
 
     return (
         <div className='mx-auto max-w-7xl  p-6 lg:px-8'>
@@ -17,7 +19,9 @@ const TvInfo = () => {
                 <>
                     <h1 className=' text-2xl'>{data.name}</h1>
                     <div>
-                        <img src={"https://image.tmdb.org/t/p/original/" + data.backdrop_path} alt='img' className='h-full' />
+                        <img src={
+                            img_condition === `https://image.tmdb.org/t/p/original/${data.backdrop_path}` ? mvimage : `https://image.tmdb.org/t/p/original/${data.backdrop_path}`
+                        } alt='img' className='h-full' />
                     </div>
                     <Cast id={data.id} type="tv" />
                 </>
