cdf02a0a95977c1831054ba41f18a1e36b7467db
darrellabuguen <darrellabuguen2@gmail.com>
2024-05-21 23:01:59 +0800

    added height limit to description in movieinfo jsx

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../components/useFetch';
 import Cast from '../components/Cast';
@@ -8,6 +8,24 @@ const MovieInfo = () => {
     const { data, loading, error } = useFetch(`https://api.themoviedb.org/3/movie/${movieid}?language=en-US`, "GET");
     var title_tag = document.querySelector("title");
     title_tag.innerText = `${moviename} | Movie Info`; //change the title
+    const [con_height, setHeight] = useState("h-16");
+    const [visible, setVisibility] = useState("block");
+    const [more, setMore] = useState("See More");
+    const checkDescriptionHeight = () => {
+        const description_con = document.querySelector(".desc_con");
+        const description = document.querySelector(".desc");
+        if (description) {
+            if (description.scrollHeight == description_con.scrollHeight) {
+                setVisibility("");
+            } else {
+                setVisibility("hidden")
+            };
+        }
+    }
+    useEffect(() => {
+        checkDescriptionHeight();
+        window.addEventListener("resize", checkDescriptionHeight);
+    });
 
     return (
         <div className='mx-auto max-w-7xl  p-6 lg:px-8'>
@@ -21,7 +39,23 @@ const MovieInfo = () => {
                             <img src={"https://image.tmdb.org/t/p/original/" + data.backdrop_path} alt='img' className='h-full' />
                             <div>
                                 <h1>Description:</h1>
-                                <p className=' text-gray-300'>{data.overview}</p>
+                                <div className={`desc_con overflow-hidden ${con_height}`}>
+                                    <p
+                                        className={`text-gray-300 desc`}
+                                    >{data.overview}</p>
+                                </div>
+                                <div
+                                    className={`text-blue-500 cursor-pointer ${visible}`}
+                                    onClick={() => {
+                                        if (con_height === "h-16") {
+                                            setHeight("max-h-full");
+                                            setMore("See Less");
+                                        } else {
+                                            setHeight("h-16");
+                                            setMore("See More");
+                                        }
+                                    }}
+                                >{more}</div>
                             </div>
                         </div>
                         <Cast id={movieid} type="movie" />
