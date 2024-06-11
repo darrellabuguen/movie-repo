721c9d93ddf5d80aa42bc140a04f383d464c6c5e
darrellabuguen <darrellabuguen2@gmail.com>
2024-06-11 13:59:31 +0800

    delete tv jsx and random jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Result from './pages/Result';
 import CelebrityInfo from './pages/CelebrityInfo';
-import Tv from './pages/Tv';
 import TvInfo from './pages/TvInfo';
 import Trending from './pages/Trending';
 import ButtonUp from './components/ButtonUp';
